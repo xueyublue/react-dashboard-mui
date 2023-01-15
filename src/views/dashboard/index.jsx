fd59fcdc409c6c443f6f841d5df4cd834789b9dc
Xue Yu <xueyublue@gmail.com>
2023-01-15 08:46:58 +0800

    Added padding to dashboard page

--- src/views/dashboard/index.jsx
+++ src/views/dashboard/index.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import { Box } from "@mui/material";
 
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <Box pl={2} pr={2}>
+      Dashboard
+    </Box>
+  );
 }
