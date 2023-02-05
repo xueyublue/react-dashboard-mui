a4633a088da017d8c404442cf2bfd695292b9af4
Xue Yu <xueyublue@gmail.com>
2023-02-05 14:02:14 +0800

    Added form page

--- src/views/form/index.jsx
+++ src/views/form/index.jsx
@@ -52,8 +52,81 @@ export default function Form() {
                   onChange={handleChange}
                   value={values.firstName}
                   name="firstName"
-                  error={!!touched}
+                  error={!!touched.firstName && !!errors.firstName}
+                  helperText={touched.firstName && errors.firstName}
+                  sx={{ gridColumn: "span 2" }}
                 />
+                <TextField
+                  fullWidth
+                  variant="filled"
+                  type="text"
+                  label="Last Name"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.lastName}
+                  name="lastName"
+                  error={!!touched.lastName && !!errors.lastName}
+                  helperText={touched.lastName && errors.lastName}
+                  sx={{ gridColumn: "span 2" }}
+                />
+                <TextField
+                  fullWidth
+                  variant="filled"
+                  type="text"
+                  label="Email"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.email}
+                  name="email"
+                  error={!!touched.email && !!errors.email}
+                  helperText={touched.email && errors.email}
+                  sx={{ gridColumn: "span 4" }}
+                />
+                <TextField
+                  fullWidth
+                  variant="filled"
+                  type="text"
+                  label="Contact Number"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.contact}
+                  name="contact"
+                  error={!!touched.contact && !!errors.contact}
+                  helperText={touched.contact && errors.contact}
+                  sx={{ gridColumn: "span 4" }}
+                />
+
+                <TextField
+                  fullWidth
+                  variant="filled"
+                  type="text"
+                  label="Address 1"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.address1}
+                  name="address1"
+                  error={!!touched.address1 && !!errors.address1}
+                  helperText={touched.address1 && errors.address1}
+                  sx={{ gridColumn: "span 4" }}
+                />
+                <TextField
+                  fullWidth
+                  variant="filled"
+                  type="text"
+                  label="Address 2"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.address2}
+                  name="address2"
+                  error={!!touched.address2 && !!errors.address2}
+                  helperText={touched.address2 && errors.address2}
+                  sx={{ gridColumn: "span 4" }}
+                />
+              </Box>
+              <Box display="flex" justifyContent="end" mt="20px">
+                <Button type="submit" color="secondary" variant="contained">
+                  Create New User
+                </Button>
               </Box>
             </form>
           )}
