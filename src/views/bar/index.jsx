4a883280a3ce6b73b2b8a8ecec4cef50fee936d5
Xue Yu <xueyublue@gmail.com>
2023-02-11 15:37:47 +0800

    Added bar chart

--- src/views/bar/index.jsx
+++ src/views/bar/index.jsx
@@ -1,5 +1,16 @@
+import { Box } from "@mui/system";
 import React from "react";
+import BarChart from "../../components/BarChart";
+import PageContainer from "../../components/PageContainer";
+import Header from "../../components/Header";
 
 export default function Bar() {
-  return <div>Bar</div>;
+  return (
+    <PageContainer>
+      <Header title="Bar Chart" subTitle="Simple Bar Chart" />
+      <Box mt={2} height="75vh">
+        <BarChart />
+      </Box>
+    </PageContainer>
+  );
 }
