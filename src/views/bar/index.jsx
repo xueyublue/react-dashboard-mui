645db7fb9696b5293f2001fb7f6ed1bfe8599d38
Xue Yu <xueyublue@gmail.com>
2023-02-11 15:39:38 +0800

    adjust bar chart height

--- src/views/bar/index.jsx
+++ src/views/bar/index.jsx
@@ -3,12 +3,15 @@ import React from "react";
 import BarChart from "../../components/BarChart";
 import PageContainer from "../../components/PageContainer";
 import Header from "../../components/Header";
+import useWindowDimension from "../../hooks/useWindowDimension";
 
 export default function Bar() {
+  const { height } = useWindowDimension();
+  const chartHeight = height - 162;
   return (
     <PageContainer>
       <Header title="Bar Chart" subTitle="Simple Bar Chart" />
-      <Box mt={2} height="75vh">
+      <Box mt={2} height={`${chartHeight}px`}>
         <BarChart />
       </Box>
     </PageContainer>
