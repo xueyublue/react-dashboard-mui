0278ab07c9c2e46751a6d519b546834b95d694e9
Xue Yu <xueyublue@gmail.com>
2023-02-13 18:45:16 +0800

    Added pie chart

--- src/views/pie/index.jsx
+++ src/views/pie/index.jsx
@@ -1,5 +1,19 @@
 import React from "react";
+import { Box } from "@mui/material";
+import useWindowDimension from "../../hooks/useWindowDimension";
+import PageContainer from "../../components/PageContainer";
+import Header from "../../components/Header";
+import PieChart from "../../components/PieChart";
 
 export default function Pie() {
-  return <div>Pie</div>;
+  const { height } = useWindowDimension();
+  const chartHeight = height - 162;
+  return (
+    <PageContainer>
+      <Header title="Pie Chart" subTitle="Simple Pie Chart" />
+      <Box mt={2} height={`${chartHeight}px`}>
+        <PieChart />
+      </Box>
+    </PageContainer>
+  );
 }
