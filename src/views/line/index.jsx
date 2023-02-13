5668dc422c407d971f9b75cf2e72324a901dd95e
Xue Yu <xueyublue@gmail.com>
2023-02-13 22:27:32 +0800

    Added line chart

--- src/views/line/index.jsx
+++ src/views/line/index.jsx
@@ -1,5 +1,19 @@
 import React from "react";
+import useWindowDimension from "../../hooks/useWindowDimension";
+import PageContainer from "../../components/PageContainer";
+import { Box } from "@mui/system";
+import LineChart from "../../components/LineChart";
+import Header from "../../components/Header";
 
 export default function Line() {
-  return <div>Line</div>;
+  const { height } = useWindowDimension();
+  const chartHeight = height - 162;
+  return (
+    <PageContainer>
+      <Header title="Line Chart" subTitle="Simple Line Chart" />
+      <Box mt={2} height={`${chartHeight}px`}>
+        <LineChart />
+      </Box>
+    </PageContainer>
+  );
 }
