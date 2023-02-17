9d154ab5daab5bae70a5c1a3ca06217eb4330883
Xue Yu <xueyublue@gmail.com>
2023-02-17 20:29:02 +0800

    Added geography chart

--- src/views/geography/index.jsx
+++ src/views/geography/index.jsx
@@ -1,5 +1,19 @@
 import React from "react";
+import PageContainer from "../../components/PageContainer";
+import { Box } from "@mui/material";
+import Header from "../../components/Header";
+import GeographyChart from "../../components/GeographyChart";
+import useWindowDimension from "../../hooks/useWindowDimension";
 
 export default function Geography() {
-  return <div>Geography</div>;
+  const { height } = useWindowDimension();
+  const chartHeight = height - 162;
+  return (
+    <PageContainer>
+      <Header title="Geography Chart" subTitle="Simple Geography Chart" />
+      <Box mt={2} height={`${chartHeight}px`}>
+        <GeographyChart />
+      </Box>
+    </PageContainer>
+  );
 }
